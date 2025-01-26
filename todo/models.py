01fdcad57b3efaca42b379dffc055cbb05786dd7
SolijonovAsadbek <solijonovasadbek25111999@gmail.com>
2025-01-26 10:49:27 +0500

    CRUD added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
-# Create model here
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
